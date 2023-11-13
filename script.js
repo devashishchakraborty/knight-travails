a7d5f1624b97666a62b61a143a5e7496bce456d0
Devashish Chakraborty <devashishchakra@gmail.com>
2023-11-13 17:40:38 +0530

    Added a BFS function to find the node

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 class Knight{
-    constructor(square){
+    constructor(square, prev = null){
+        this.prev = prev;
         this.square = square;
         this.next = [];
     }
@@ -8,56 +9,67 @@ class Knight{
 class Gameboard{
     constructor(knight){
         this.root = knight;
-        this.board = this.createBoard(knight.square);
     }
 
-    createBoard(square){
-        let board = [];
-        for (let i = 0; i < 8; i++){
-            board.push(new Array(8));
+    calculatePossibleMoves(knight){
+        if (knight.square[0] - 2 >= 0 && knight.square[1] - 1 >= 0){
+            knight.next.push(new Knight([knight.square[0] - 2, knight.square[1] - 1], knight));
         }
-        board[square[0]][square[1]] = 'k';
-        return board;
+    
+        if (knight.square[0] - 1 >= 0 && knight.square[1] - 2 >= 0){
+            knight.next.push(new Knight([knight.square[0] - 1, knight.square[1] - 2], knight));
+        }
+    
+        if (knight.square[0] - 2 >= 0 && knight.square[1] + 1 <= 7){
+            knight.next.push(new Knight([knight.square[0] - 2, knight.square[1] + 1], knight));
+        }
+    
+        if (knight.square[0] + 1 <= 7 && knight.square[1] - 2 >= 0){
+            knight.next.push(new Knight([knight.square[0] + 1, knight.square[1] - 2], knight));
+        }
+    
+        if (knight.square[0] - 1 >= 0 && knight.square[1] + 2 <= 7){
+            knight.next.push(new Knight([knight.square[0] - 1, knight.square[1] + 2], knight));
+        }
+    
+        if (knight.square[0] + 2 <= 7 && knight.square[1] - 1 >= 0){
+            knight.next.push(new Knight([knight.square[0] + 2, knight.square[1] - 1], knight));
+        }
+        
+        if (knight.square[0] + 1 <= 7 && knight.square[1] + 2 <= 7){
+            knight.next.push(new Knight([knight.square[0] + 1, knight.square[1] + 2], knight));
+        }
+        
+        if (knight.square[0] + 2 <= 7 && knight.square[1] + 1 <= 7){
+            knight.next.push(new Knight([knight.square[0] + 2, knight.square[1] + 1], knight));
+        }
+        return knight;
+    }
+
+    breadthFirstSearch(square){
+        let Q = [this.root];
+        while(Q.length !== 0){
+            let current = Q[0];
+            if (current.square[0] === square[0] && 
+                current.square[1] === square[1]) return current;
+            Q = [...Q, ...this.calculatePossibleMoves(current).next];
+            Q.shift();
+        }
     }
 }
 
-function calculatePossibleMoves(knight){
-    if (knight.square[0] - 2 >= 0 && knight.square[1] - 1 >= 0){
-        knight.next.push(new Knight([knight.square[0] - 2, knight.square[1] - 1]));
-    }
 
-    if (knight.square[0] - 1 >= 0 && knight.square[1] - 2 >= 0){
-        knight.next.push(new Knight([knight.square[0] - 1, knight.square[1] - 2]));
+function knightMoves(start, end){
+    let gameboard = new Gameboard(new Knight(start));
+    let node = gameboard.breadthFirstSearch(end);
+    let route = [];
+    while(node !== null){
+        route.push(node.square);
+        node = node.prev;
     }
-
-    if (knight.square[0] - 2 >= 0 && knight.square[1] + 1 <= 7){
-        knight.next.push(new Knight([knight.square[0] - 2, knight.square[1] + 1]));
-    }
-
-    if (knight.square[0] + 1 <= 7 && knight.square[1] - 2 >= 0){
-        knight.next.push(new Knight([knight.square[0] + 1, knight.square[1] - 2]));
-    }
-
-    if (knight.square[0] - 1 >= 0 && knight.square[1] + 2 <= 7){
-        knight.next.push(new Knight([knight.square[0] - 1, knight.square[1] + 2]));
-    }
-
-    if (knight.square[0] + 2 <= 7 && knight.square[1] - 1 >= 0){
-        knight.next.push(new Knight([knight.square[0] + 2, knight.square[1] - 1]));
-    }
-    
-    if (knight.square[0] + 1 <= 7 && knight.square[1] + 2 <= 7){
-        knight.next.push(new Knight([knight.square[0] + 1, knight.square[1] + 2]));
-    }
-    
-    if (knight.square[0] + 2 <= 7 && knight.square[1] + 1 <= 7){
-        knight.next.push(new Knight([knight.square[0] + 2, knight.square[1] + 1]));
-    }
-    return knight;
+    return route.reverse();
 }
 
-let knight = new Knight([7, 4]);
-let gameboard = new Gameboard(knight);
+let route = knightMoves([3,3],[4,3]);
 
-calculatePossibleMoves(knight);
-console.log(knight.next);
+console.log(`You made it in ${route.length - 1} moves! \nHere's your path:`, JSON.stringify(route));
